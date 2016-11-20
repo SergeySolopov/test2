15f1affa30ff08438c7a4b5a5bb52bfdc4a50de0
Sergey Solopov <solopov.biz@gmail.com>
2016-11-21 01:25:32 +0300

    2b

--- src/index.js
+++ src/index.js
@@ -47,7 +47,7 @@ app.get('/', (req, res) => {
 
 app.get('/2a/', (req, res) => {
 
-   // res.sendStatus(200);
+    // res.sendStatus(200);
 
     let a = +(!!req.query && 'a' in req.query ? req.query.a : 0);
     let b = +(!!req.query && 'b' in req.query ? req.query.b : 0);
@@ -55,6 +55,53 @@ app.get('/2a/', (req, res) => {
 
     console.log(`a = ${a} & b = ${b} result = ${r}`);
     return res.send('' + r);
+
+});
+
+app.get('/2b/', (req, res) => {
+
+    // res.sendStatus(200);
+
+    let r = 'Invalid fullname';
+    let fullname = ('query' in req && !!req.query.fullname ? req.query.fullname : '');
+    let m = fullname.split(' ');
+
+    //ИОФ , ИФ, Ф  => ФИО
+    switch (m.length) {
+        case 3:
+        {
+            r = m[2] + ' ' + m[0].substr(0, 1) + '. ' + m[1].substr(0, 1) + '.';
+            break;
+        }
+        case 2:
+        {
+            r = m[1] + ' ' + m[0].substr(0, 1) + '.';
+            break;
+        }
+        case 1:
+        {
+            if (m[0].length) {
+                r = m[0];
+            }
+            break;
+        }
+    }
+
+    var bOk = true;
+    m.forEach((i) => {
+        if(/\d+/.test(i))
+        {
+            bOk = false;
+        }
+    });
+    if(!bOk)
+    {
+        r = 'Invalid fullname';
+    }
+    
+
+    console.log(`${r} => ${fullname}`);
+    return res.send(r);
 
 });
 
