6968a9849347c5ecbebc0de2094804c20288a3f2
Sergey Solopov <solopov.biz@gmail.com>
2016-11-21 00:40:22 +0300

    2a

--- src/index.js
+++ src/index.js
@@ -2,28 +2,31 @@ import canonize from './canonize';
 import express from 'express';
 import corsAccess from 'cors';
 
-//
-const arUrl = [
-    'username=https://vk.com/skillbranch',
-    'username=//vk.com/skillbranch',
-    'username=skillbranch',
-    'username=https://vk.com/skillbranch?w=wall-117903599_1076'
-];
-
-
-arUrl.forEach((url) => {
-    let un = canonize(url);
-    if (!!un) {
-        console.log('@' + un);
-    }
-    else {
-        console.log('Invalid username');
-    }
-});
 
 let app = express();
 app.use(corsAccess());
 app.get('/', (req, res) => {
+
+
+//
+//    const arUrl = [
+//        'username=https://vk.com/skillbranch',
+//        'username=//vk.com/skillbranch',
+//        'username=skillbranch',
+//        'username=https://vk.com/skillbranch?w=wall-117903599_1076'
+//    ];
+//
+//
+//    arUrl.forEach((url) => {
+//        let un = canonize(url);
+//        if (!!un) {
+//            console.log('@' + un);
+//        }
+//        else {
+//            console.log('Invalid username');
+//        }
+//    });
+
 
     let url = req.query.username;
     let un = canonize(url);
@@ -38,7 +41,20 @@ app.get('/', (req, res) => {
     //    "url": url,
     //    "username": username
     //});
-     return res.send(username);
+    return res.send(username);
+
+});
+
+app.get('/2a/', (req, res) => {
+
+   // res.sendStatus(200);
+
+    let a = +(!!req.query && 'a' in req.query ? req.query.a : 0);
+    let b = +(!!req.query && 'b' in req.query ? req.query.b : 0);
+    let r = a + b;
+
+    console.log(`a = ${a} & b = ${b} result = ${r}`);
+    return res.send('' + r);
 
 });
 
